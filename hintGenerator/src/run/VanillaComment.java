b20586fa82461899105f30259122ac4649f54539
chrispiech <cpiech@stanford.edu>
2014-11-04 19:11:10 +0000

    a

---
+++ hintGenerator/src/run/VanillaComment.java
@@ -0,0 +1,69 @@
+package run;
+
+import java.io.File;
+import java.util.*;
+
+import editDistance.TreeEditDistance;
+import editDistance.TreeMove;
+import editDistance.TreeReplace;
+import minions.BlocklyHelper;
+import minions.GraphLoader;
+import minions.ProgramLoader;
+import minions.XmlIdMap;
+import models.ast.Program;
+import models.ast.Tree;
+import util.DirectedGraph;
+import util.FileSystem;
+import util.Warnings;
+
+public class VanillaComment {
+
+	private static final String ASSN_ID = "hoc4";
+
+	private Map<String, Program> programMap;
+	private DirectedGraph<String> policy;
+	
+	private void run() {
+		FileSystem.setAssnId(ASSN_ID);
+		
+		// First load all of the programs..
+		List<Program> programs = ProgramLoader.loadPrograms(1000);
+		programMap = new HashMap<String, Program>();
+		for(Program p : programs) {
+			String id = p.getId();
+			programMap.put(id, p);
+		}
+		
+		// Then make a comment for each edge in the policy.
+		policy = GraphLoader.loadPolicy("nodeRank.txt");
+		for(String currId : policy.outgoingSet()) {
+			Set<String> outgoing = policy.getOutgoing(currId);
+			Warnings.check(outgoing.size() == 1, "must be 1");
+			String nextId = outgoing.iterator().next();
+			makeComment(currId, nextId);
+		}
+	}
+
+	private void makeComment(String currId, String nextId) {
+		Tree a = programMap.get(currId).getAst();
+		Tree b = programMap.get(nextId).getAst();
+		List<TreeMove> treeMoves = TreeEditDistance.calcMoves(a, b);
+		
+		System.out.println("\n");
+		System.out.println("Here is an example edit!");
+		System.out.println("original:    " + a);
+		System.out.println("policy next: " + b);
+
+		Warnings.check(treeMoves.size() == 1, "should be 1");
+		TreeMove move = treeMoves.get(0);
+		System.out.println("move: " + move);
+		
+		System.out.println("\n");
+		throw new RuntimeException("this is just a test");
+	}
+
+	public static void main(String[] args) {
+		new VanillaComment().run();
+	}
+
+}
